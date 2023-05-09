d52e0ca4a3d84f00b9094a4b2600568297d4dcf1
Niranjan <pniranjn18@gmail.com>
2023-05-09 16:25:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bustracker/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.bustracker
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+}
